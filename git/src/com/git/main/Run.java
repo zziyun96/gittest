1abb294d6c06b587161e060735cb12c1c898d9a5
hwang <hwang@LAPTOP-FIE63JEP>
2021-12-17 10:01:25 +0900

    Run클래스 main메소드에 변수추가 -bs-

--- git/src/com/git/main/Run.java
+++ git/src/com/git/main/Run.java
@@ -5,6 +5,19 @@ public class Run {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("나의 첫 git실행!");
+		
+		int age=19;
+		String name="유병승";
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
