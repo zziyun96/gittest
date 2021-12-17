c98760c8439f72fa9236fa238004574ca849a773
hwang <hwang@LAPTOP-FIE63JEP>
2021-12-17 10:51:02 +0900

    Member클래스 생성자, get/set 추가 -jy-

--- git/src/com/git/model/vo/Member.java
+++ git/src/com/git/model/vo/Member.java
@@ -5,4 +5,35 @@ public class Member {
 	private int age;
 	private char gender;
 	
+	public Member(String name, int age, char gender) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
+
+	public Member() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	
 }
